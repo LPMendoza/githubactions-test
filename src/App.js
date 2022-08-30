c260b39a964a8503242decf044995c1f992d0348
LPMendoza <lpedromendoza333@gmail.com>
2022-08-29 23:01:23 -0500

    asdas

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>DEPLOYED</h1>
+        <h1>DEPLOYED HEROKU</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
